061f65b26b3ca7fcfac69c5adb58f882047a3ae8
Hamidmath <39564618+Hamidmath@users.noreply.github.com>
2020-03-25 01:19:31 +0430

    Update data.js

--- data.js
+++ data.js
@@ -83,10 +83,100 @@ branch: "Matematik",
 sectionName: "Modüler Aritmetik",
 session: "2"
 },
-//--------------------14 
+//--------------------15 
 {
 branch: "Matematik",
 sectionName: "Problemler",
+session: "2"
+},
+//--------------------16 
+{
+branch: "Matematik",
+sectionName: "Polinomlar",
+session: "1"
+},
+//--------------------17 
+{
+branch: "Matematik",
+sectionName: "II. Dereceden Denklemler",
+session: "2"
+},
+//--------------------18 
+{
+branch: "Matematik",
+sectionName: "II. Dereceden  Eşitsizlikler",
+session: "2"
+},
+//--------------------19
+{
+branch: "Matematik",
+sectionName: "Parabol",
+session: "2"
+},
+//--------------------20 
+{
+branch: "Matematik",
+sectionName: "Trigonometri",
+session: "2"
+},
+//--------------------21 
+{
+branch: "Matematik",
+sectionName: "Karmaşık Sayılar",
+session: "2"
+},
+//--------------------22 
+{
+branch: "Matematik",
+sectionName: "Logaritma",
+session: "2"
+},
+//--------------------23 
+{
+branch: "Matematik",
+sectionName: "Toplam ve Çarpım Sembolü",
+session: "2"
+},
+//--------------------24 
+{
+branch: "Matematik",
+sectionName: "Özel Tanımlı Fonksiyonlar",
+session: "2"
+},
+//--------------------25
+{
+branch: "Matematik",
+sectionName: "Limit",
+session: "2"
+},
+//--------------------26 
+{
+branch: "Matematik",
+sectionName: "Diziler ve Seriler",
+session: "2"
+},
+//--------------------27 
+{
+branch: "Matematik",
+sectionName: "Türev",
+session: "2"
+},
+//--------------------28
+{
+branch: "Matematik",
+sectionName: "İntegral",
+session: "2"
+},
+//--------------------29 
+{
+branch: "Matematik",
+sectionName: "Matris",
+session: "2"
+},
+//--------------------30 
+{
+branch: "Matematik",
+sectionName: "Permütasyon-Kombinasyon Binom-Olasılık",
 session: "2"
 }
 ];
